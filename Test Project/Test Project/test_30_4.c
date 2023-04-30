a1bffd6bb7c1a136ffec4e48f115809f473cc386
nobodyyangs <1922491200@qq.com>
2023-04-30 22:59:22 +0800

    提交

---
+++ Test Project/Test Project/test_30_4.c
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+
+//int main()
+//{
+//	int ch = 0;
+//	int ret = 0;
+//	char password[20] = { 0 };
+//	printf("请输入密码：");
+//	scanf("%s", &password);
+//	while ((ch = getchar()) != '\n')
+//	{
+//		;
+//	}
+//	printf("请确认（Y/N）：");
+//	ret = getchar();
+//	if (ret == 'Y')
+//	{
+//		printf("已确认\n");
+//		//printf("passwword：%s", password);
+//	}
+//	else
+//	{
+//		printf("已放弃\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int ch = 0;
+//	while ((ch = getchar()) != EOF)
+//	{
+//		if (ch < '0' || ch>'9')
+//			continue;
+//		putchar(ch);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 10; i++)
+//	{
+//		if (i == 5)
+//			break;
+//		printf("%d ", i);
+//	}
+//	return 0;
+//}
