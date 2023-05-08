c1e1ace175ae7dc87389e3c63d7b7a1f41fc041b
nobodyyangs <1922491200@qq.com>
2023-05-08 22:40:43 +0800

    提交 2023.5.8

---
+++ Test Project/Test Project/test_8_5.c
@@ -0,0 +1,145 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<math.h>
+#include<Windows.h>
+#include<time.h>
+
+////int is_prime(int n)
+////{
+////	int j = 0;
+////	for (j = 2; j < n; j++)
+////	{
+////		if (n % j == 0)
+////		{
+////			return 0;
+////		}
+////	}
+////	return 1;
+////}
+//int is_prime(int n)
+//{
+//	int j = 0;
+//	for (j = 2; j <= sqrt(n); j++)
+//	{
+//		if (n % j == 0)
+//			return 0;
+//	}
+//	return 1;
+//}
+//int main()
+//{
+//	int i = 0;
+//	for (i = 100; i <= 200; i++)
+//	{
+//		if (is_prime(i) == 1)
+//			printf("%d ", i);
+//	}
+//	return 0;
+//}
+
+//int is_leap_year(int n)
+//{
+//	if ((n % 4 == 0 && n % 100 != 0) || (n % 400 == 0))
+//		return 1;
+//	else
+//		return 0;
+//}
+//int main()
+//{
+//	int year = 0;
+//	for (year = 1000; year <= 2023; year++)
+//	{
+//		if(1==is_leap_year(year))
+//		{
+//			printf("%d ", year);
+//		}
+//	}
+//	return 0;
+//}
+
+
+//int binary_search(int arr[], int k,int sz)
+//{
+//	int left = 0;
+//	int right = sz - 1;
+//	while (left <= right)
+//	{
+//		int mid = (left + right) / 2;
+//		if (arr[mid] < k)
+//		{
+//			left = mid + 1;
+//		}
+//		else if (arr[mid] > k)
+//		{
+//			right = mid - 1;
+//		}
+//		else
+//		{
+//			return mid;
+//		}
+//	}
+//	return -1;
+//}
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int k = 17;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int ret = binary_search(arr, k,sz);
+//	if (ret == -1)
+//	{
+//		printf("找不到\n");
+//	}
+//	else
+//	{
+//		printf("找到了，下标是：%d\n", ret);
+//	}
+//	return 0;
+//}
+
+//void Add(int* p)
+//{
+//	++*p;
+//}
+//int main()
+//{
+//	int num = 0;
+//	Add(&num);
+//	printf("num = %d\n", num);
+//	Add(&num);
+//	printf("num = %d\n", num);	
+//	Add(&num);
+//	printf("num = %d\n", num);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int len = 0;
+//	//len = strlen("abc");
+//	//printf("%d\n", len);
+//	printf("%d\n", strlen("abc");
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("%d", printf("%d", printf("%d", 43)));
+//	return 0;
+//}
+
+//int Add(int x, int y)
+//{
+//	int z = x + y;
+//	return z;
+//}
+#include"add_8_5.h"
+int main()
+{
+	int a = 10;
+	int b = 20;
+	printf("%d\n", Add(a, b));
+	return 0;
+}
