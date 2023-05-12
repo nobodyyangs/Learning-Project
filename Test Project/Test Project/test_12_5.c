1de65ce673be12ed9b3ea61859c98d03f8f87c92
nobodyyangs <1922491200@qq.com>
2023-05-12 20:54:35 +0800

    提交2023.5.12

---
+++ Test Project/Test Project/test_12_5.c
@@ -0,0 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+#include<time.h>
+#include<Windows.h>
+
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	printf("%d\n", sizeof(arr));
+//	printf("%d\n", strlen(arr));
+//	return 0;
+//}
+
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	int i = 0;
+//	for (i = 0; i < (int)strlen(arr); i++)
+//	{
+//		printf("%c ", arr[i]);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,0 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", arr[i]);
+//		printf("\n&arr[%d] = %p\n", i, &arr[i]);
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[3][4] = { {1,2,3},{4,5} };
+//	int i = 0;
+//	for (i = 0; i < 3; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < 4; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	for (i = 0; i < 3; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < 4; j++)
+//		{
+//			printf("\narr[%d][%d] = %p ", i, j, &arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
