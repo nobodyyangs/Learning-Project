d1f42a9f61e95cd44ebb000cd45f28a370b85692
nobodyyangs <1922491200@qq.com>
2023-05-25 20:42:12 +0800

    提交2023.5.25

---
+++ Test Project/Test Project/test_25_5.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//int main()
+//{
+//	int a = 5 / 2;
+//	printf("a = %d\n", a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 16;
+//	int b = a >> 1;
+//	printf("b = %d\n", b);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = -1;
+//	int b = a >> 1;
+//	printf("b = %d\n", b);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 5;
+//	int b = a << 1;
+//	printf("b = %d\n", b);
+//	return 0;
+//}
