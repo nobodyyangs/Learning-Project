ab65d12a64105f7b36a323c086588d305bc30bf4
nobodyyangs <1922491200@qq.com>
2023-05-27 17:17:59 +0800

    提交2023.5.27

---
+++ Test Project/Test Project/test_27_5.c
@@ -0,0 +1,86 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	int c = a & b;
+//	printf("c = %d", c);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	int c = a | b;
+//	printf("c = %d", c);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	int c = a ^ b;
+//	printf("c = %d", c);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 3;
+//	int b = 5;
+//	printf("before: a=%d, b=%d\n", a, b);
+//	a = a ^ b;
+//	b = a ^ b;
+//	a = a ^ b;
+//	printf("after: a=%d, b=%d\n", a, b);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int num = 0;
+//	int count = 0;
+//	scanf("%d", &num);
+//	//while (num)
+//	//{
+//	//	if (num % 2 == 1)
+//	//	{
+//	//		count++;
+//	//	}
+//	//	num = num / 2;
+//	//}
+//	int i = 0;
+//	for (i = 0; i < 32; i++)
+//	{
+//		if (((num >> i) & 1) == 1)
+//		{
+//			count++;
+//		}
+//	}
+//	printf("count = %d", count);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 1;
+//	printf("%d\n", !a);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 0;
+//	if (a)
+//	{
+//		printf("o\n");
+//	}
+//	if (!a)
+//	{
+//		printf("x\n");
+//	}
+//}
